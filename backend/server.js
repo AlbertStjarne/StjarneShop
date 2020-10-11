7e4b6e3dd3ee7ae1aa32676f6c90da2c00766b5e
Albert Stjärne <albert.stjarne@gmail.com>
2020-10-11 20:12:54 +0200

    adds middleware folder with errorMiddleware file, moving error handling here, applying these middleware in the server file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
@@ -17,6 +18,12 @@ app.get('/', (req, res) => {
 
 // mounting, anything going to api/products goes to productRoutes
 app.use('/api/products', productRoutes);
+
+// middleware for routes not found (404)
+app.use(notFound);
+
+// error handler middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
