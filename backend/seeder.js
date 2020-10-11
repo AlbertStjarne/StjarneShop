4ebd800215d5f0dea5b34aa027a7bfbfb0848c04
Albert Stjärne <albert.stjarne@gmail.com>
2020-10-11 15:10:21 +0200

    adds seeder.js file with script to seed db with sample data, adds script to import and destroy seeding data in the db

---
+++ backend/seeder.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from './data/users.js';
+import products from './data/products.js';
+import User from './models/userModel.js';
+import Product from './models/productModel.js';
+import Order from './models/orderModel.js';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+  try {
+    // clearing db before inserting
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    // inserting to db
+    const createdUsers = await User.insertMany(users);
+
+    const adminUser = createdUsers[0]._id;
+
+    const sampleProducts = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log('Sample data imported!'.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+
+    console.log('Sample data destroyed!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+// looking for -d in the console input to delete else import
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}
