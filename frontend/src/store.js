64603331707792f54cb4aded5492121684cd0f8f
Albert Stjärne <albert.stjarne@gmail.com>
2020-10-21 18:15:26 +0200

    adds userConstants file with contstants, adds userReducers file with userLoginReducer, adds userLoginReducer to the store and pulling any existing user from local storage, adds userActions file with login action posting user data and saving user to local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,12 +6,14 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 // combining the different reducers
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 // variable for cartItems to add to initial state
@@ -19,9 +21,15 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+// variable for userInfo to add to initial state, from local storage
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 // state set when the redux store gets loaded
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 // middleware that wi
