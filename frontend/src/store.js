790932ebe1447aef556ae3fafdd72fea0af71ed8
Albert Stjärne <albert.stjarne@gmail.com>
2020-11-03 08:27:58 +0100

    adds shippingScreen with address form, adds action to saveShippingAddress, incl shippingAddress in the cart reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -34,9 +34,16 @@ const userInfoFromStorage = localStorage.getItem('userInfo')
   ? JSON.parse(localStorage.getItem('userInfo'))
   : null;
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
+  ? JSON.parse(localStorage.getItem('shippingAddress'))
+  : {};
+
 // state set when the redux store gets loaded
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
