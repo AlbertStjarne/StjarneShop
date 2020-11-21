f0521e4c69031eedae7a21868278a33fa20fc5d9
Albert Stjärne <albert.stjarne@gmail.com>
2020-11-21 10:49:54 +0100

    adds getUsers route in userController, adds admin middleware function to check that user is admin, adds getUsers route to appl root route with protect and admin middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,10 +5,12 @@ import {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
 } from '../controllers/userController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser);
+// using both protect and admin middleware when getting users
+router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 router
   .route('/profile')
