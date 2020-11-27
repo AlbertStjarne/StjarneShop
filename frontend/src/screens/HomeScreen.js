123b22f913022d1e1d4674cc5ca7543b31ea43c5
Albert Stjärne <albert.stjarne@gmail.com>
2020-11-27 16:17:49 +0100

    adds SearchBox component taking in search keyword and add it to the Header

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,7 +6,9 @@ import Loader from '../components/Loader';
 import Message from '../components/Message';
 import { listProducts } from '../actions/productActions';
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword;
+
   const dispatch = useDispatch();
 
   // getting productList from state
@@ -15,8 +17,8 @@ const HomeScreen = () => {
   const { loading, error, products } = productList;
 
   useEffect(() => {
-    dispatch(listProducts());
-  }, [dispatch]);
+    dispatch(listProducts(keyword));
+  }, [dispatch, keyword]);
 
   return (
     <>
